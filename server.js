0b8c1c4a4fb898265a7b5bcdfdd470cd2d3efafa
Edu Aguilar <eduardoaguilartirado@gmail.com>
2026-01-26 20:30:33 +0100

    chore: project setup: express, mongoose and docker

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
+import express from 'express'
+import mongoose from 'mongoose'
 
-console.log("server.js")
+const app = express();
+
+try {
+  await mongoose.connect('mongodb://admin:admin123@localhost:27017/db?authSource=admin');
+  console.log('mongodb connected =)');
+  
+} catch (error) {
+  console.log("mongodb error: ", error);
+}
+
+app.get('/example', (request, response) => {
+  response.json({
+    info: "first endpoint"
+  })
+})
+
+app.listen(3000, () => {
+  console.log("up & running on port: ", 3000);
+})
