b6177c3a2d8d400eb309dbfa56569be741a337a4
Edu Aguilar <eduardoaguilartirado@gmail.com>
2026-01-27 21:49:19 +0100

    solution: e2e test about creating products

--- src/tests/products/product-create.test.ts
+++ src/tests/products/product-create.test.ts
@@ -1,4 +1,21 @@
+import request from "supertest";
+import { app } from "../../api";
+
 describe("POST /products/:id", () => {
-  test("Given no name or description, endpoint should return a 400 error", () => {});
-  test("When data is right, product should be created", () => {});
+  test("Given no name or description, endpoint should return a 400 error", async () => {
+    const response = await request(app).post("/products").send({
+      name: "test",
+    });
+
+    expect(response.status).toBe(400);
+  });
+
+  test("When data is right, product should be created", async () => {
+    const response = await request(app).post("/products").send({
+      name: "test",
+      description: "description",
+    });
+
+    expect(response.status).toBe(201);
+  });
 });
