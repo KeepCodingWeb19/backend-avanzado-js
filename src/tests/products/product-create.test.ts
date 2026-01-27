d2bdd856bce9e88560ae7ed88e4ae689e44bda8d
Edu Aguilar <eduardoaguilartirado@gmail.com>
2026-01-27 21:35:30 +0100

    exercise: e2e test about creating products

---
+++ src/tests/products/product-create.test.ts
@@ -0,0 +1,4 @@
+describe("POST /products/:id", () => {
+  test("Given no name or description, endpoint should return a 400 error", () => {});
+  test("When data is right, product should be created", () => {});
+});
