cc85ed919700b27334457bb016dd4bec640316bc
Edu Aguilar <eduardoaguilartirado@gmail.com>
2026-02-02 23:03:18 +0100

    feat: validate data using zod

--- src/ui/controllers/product/update-product-controller.ts
+++ src/ui/controllers/product/update-product-controller.ts
@@ -1,23 +1,37 @@
 import { ProductMongodbRepository } from '../../../infrastructure/repositories/product-mongo-repository';
 import { UpdateProductUseCase } from './../../../domain/use-cases/product/update-product-usecase';
 import { Request, Response } from 'express';
+import * as z from 'zod';
+
+const updateProductParamsValidator = z.object({
+  productId: z.string(),
+});
+
+const updateProductBodyValidator = z.object({
+  name: z.string().min(3).optional(),
+  description: z.string().min(10).max(150).optional(),
+});
+
+const userRequestValidator = z.object({
+  id: z.string(),
+});
 
 export const updateProductController = async (request: Request, response: Response) => {
-  const { productId } = request.params;
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { name, description }: { name: string; description: string } = request.body;
+  const { productId } = updateProductParamsValidator.parse(request.params);
+  const { name, description } = updateProductBodyValidator.parse(request.body);
+  const { id: userId } = userRequestValidator.parse(request.user);
 
   const productMongoDbRepository = new ProductMongodbRepository();
   const updateProductUseCase = new UpdateProductUseCase(productMongoDbRepository);
 
   try {
     const updatedProduct = await updateProductUseCase.execute(
-      productId as string,
+      productId,
       {
         name,
         description,
       },
-      request.user?.id ?? ''
+      userId
     );
     response.json({
       content: updatedProduct,
